e14d8d5ae3470ebcf23275bb61b0f77d1fdcce63
nickchangs <nickchangs@gmail.com>
2017-06-01 11:30:40 +0800

    Update zbx-agent-install.sh

--- zbx-agent-install.sh
+++ zbx-agent-install.sh
@@ -1,60 +1,34 @@
-#this is Bash install Zabbix Agent flow
-#and Check servername and Hostname
-
-
 #!/bin/bash
 read -p "Input hostname you want to change : " NewName
 
-rm -rf /etc/zabbix/zabbix_agentd.conf
+rm -rf /etc/zabbix/*.sh*
+rm -rf /etc/zabbix/*.conf*
+rm -rf /etc/zabbix/zabbix_agentd.d/*
 yum install net-tools bind-utils wget -y
 rpm -Uvh http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-agent-3.2.6-1.el7.x86_64.rpm
 chkconfig zabbix-agent on
 
 setenforce 0
 sed -i "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config
-wget https://github.com/nickchangs/zabbix-agent2/blob/master/ngx_status.sh -P /etc/zabbix/
-wget https://github.com/nickchangs/zabbix-agent2/blob/master/access_status.sh -P /etc/zabbix/
-wget https://github.com/nickchangs/zabbix-agent2/blob/master/ip_connection_count.sh -P /etc/zabbix/
-wget https://github.com/nickchangs/zabbix-agent2/blob/master/zabbix_agentd.conf -P /etc/zabbix/
-wget https://github.com/nickchangs/zabbix-agent2/blob/master/userparameter_nginx -P /etc/zabbix/zabbix_agentd.d/
+
+curl -s https://raw.githubusercontent.com/nickchangs/zabbix-agent2/master/ngx_status.sh -o "/etc/zabbix/ngx_status.sh"
+curl -s https://raw.githubusercontent.com/nickchangs/zabbix-agent2/master/access_status.sh -o "/etc/zabbix/access_status.sh"
+curl -s https://raw.githubusercontent.com/nickchangs/zabbix-agent2/master/ip_connection_count.sh -o "/etc/zabbix/ip_connection_count.sh"
+curl -s https://raw.githubusercontent.com/nickchangs/zabbix-agent2/master/zabbix_agentd.conf -o "/etc/zabbix/zabbix_agentd.conf"
+curl -s https://raw.githubusercontent.com/nickchangs/zabbix-agent2/master/userparameter_nginx -o "/etc/zabbix/zabbix_agentd.d/userparameter_nginx.conf"
 chmod +x /etc/zabbix/*.sh
 
-#if Agent is passive mode 
+#if Agent is passive mode
 #sed -i "s/Server=127.0.0.1/Server=61.216.144.186/g" /etc/zabbix/zabbix_agentd.conf
 #sed -i "s/# ListenPort=10050/ListenPort=9345/g" /etc/zabbix/zabbix_agentd.conf
 
 #if Agent is active mode
 #sed -i "s/ServerActive=127.0.0.1/ServerActive=61.216.144.184:10051/g" /etc/zabbix/zabbix_agentd.conf
-#echo "StartAgents=0"  >> /etc/zabbix/zabbix_agentd.conf 
+#echo "StartAgents=0"  >> /etc/zabbix/zabbix_agentd.conf
 #echo "RefreshActiveChecks=60" >> /etc/zabbix/zabbix_agentd.conf
 #echo "UserParameter=nginx.status[*],/etc/zabbix/ngx_status.sh \$1" >> /etc/zabbix/zabbix_agentd.conf
 #echo "UserParameter=netstat.stat[*],(netstat -ant |grep -i $1|wc -l)" >> /etc/zabbix/zabbix_agentd.conf
 #echo "UserParameter=access.status[*],/etc/zabbix/access_status.sh \$1" >> /etc/zabbix/zabbix_agentd.conf
-echo "Hostname=$NewName" >> /etc/zabbix/zabbix_agentd.conf
+echo Hostname=$NewName >> /etc/zabbix/zabbix_agentd.conf
 systemctl enable zabbix-agent
-service zabbix-agent restart
-
-#mkdir /var/log/ip_connection
-#echo "*/3 * * * * sh /etc/zabbix/ip_connection_count.sh" >> /var/spool/cron/root
-#service crond restart
-#
-##Check
-#rpm -qa | grep zabbix
-#ll /etc/zabbix/
-#tail -n 3 /etc/zabbix/zabbix_agentd.conf
-#ll /var/log/
-#cat /var/spool/cron/root
-#cat /etc/zabbix/zabbix_agentd.conf | grep Hostname=
-#rm -rf /root/test
-#
-#stsList=`cat /opt/APP/openresty/nginx/conf/vhost/*.conf | grep server_name | grep -v "#"| sed "s/ /\n/g" | grep cdn > list`
-#Total=`cat list | wc -l`
-#for (( i=1 ; i<=$Total ; i=i+1 ))
-#do
-#        HostName=`cat list | head -n $i | tail -n 1`
-#        ping -c 1 -W 1 $HostName | awk '{print $3}' | tr -d "(" | tr -d ")" > Result
-#        HostIP=`head -n 1 Result`
-#        echo "$HostName be resolv IP : $HostIP"
-#done
-#
-#rm -rf test list
+service zabbix-agent restart               
