020798f971848870300f5dc152c1696bc52c31ad
nickchangs <nickchangs@gmail.com>
2017-06-01 10:29:56 +0800

    Update zbx-agent-install.sh

--- zbx-agent-install.sh
+++ zbx-agent-install.sh
@@ -12,10 +12,11 @@ chkconfig zabbix-agent on
 
 setenforce 0
 sed -i "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config
-wget https://www.dropbox.com/s/h44nyy2ylnqurvh/ngx_status.sh
-wget https://www.dropbox.com/s/anhiq85hrac030p/access_status.sh
-wget https://www.dropbox.com/s/3dayo61kyt31t6m/ip_connection_count.sh
-mv *.sh /etc/zabbix/
+wget https://github.com/nickchangs/zabbix-agent2/blob/master/ngx_status.sh -P /etc/zabbix/
+wget https://github.com/nickchangs/zabbix-agent2/blob/master/access_status.sh -P /etc/zabbix/
+wget https://github.com/nickchangs/zabbix-agent2/blob/master/ip_connection_count.sh -P /etc/zabbix/
+wget https://github.com/nickchangs/zabbix-agent2/blob/master/zabbix_agentd.conf -P /etc/zabbix/
+wget https://github.com/nickchangs/zabbix-agent2/blob/master/userparameter_nginx -P /etc/zabbix/zabbix_agentd.d/
 chmod +x /etc/zabbix/*.sh
 
 #if Agent is passive mode 
@@ -23,38 +24,37 @@ chmod +x /etc/zabbix/*.sh
 #sed -i "s/# ListenPort=10050/ListenPort=9345/g" /etc/zabbix/zabbix_agentd.conf
 
 #if Agent is active mode
-sed -i "s/ServerActive=127.0.0.1/ServerActive=61.216.144.184:10051/g" /etc/zabbix/zabbix_agentd.conf
-echo "StartAgents=0"  >> /etc/zabbix/zabbix_agentd.conf 
-echo "RefreshActiveChecks=60" >> /etc/zabbix/zabbix_agentd.conf
-echo "UserParameter=nginx.status[*],/etc/zabbix/ngx_status.sh \$1" >> /etc/zabbix/zabbix_agentd.conf
-echo "UserParameter=netstat.stat[*],(netstat -ant |grep -i $1|wc -l)" >> /etc/zabbix/zabbix_agentd.conf
-echo "UserParameter=access.status[*],/etc/zabbix/access_status.sh \$1" >> /etc/zabbix/zabbix_agentd.conf
-HostName=`cat /etc/zabbix/zabbix_agentd.conf | grep Hostname= | grep -v "#"`
-sed -i "s/$HostName/Hostname=$NewName/g" /etc/zabbix/zabbix_agentd.conf
+#sed -i "s/ServerActive=127.0.0.1/ServerActive=61.216.144.184:10051/g" /etc/zabbix/zabbix_agentd.conf
+#echo "StartAgents=0"  >> /etc/zabbix/zabbix_agentd.conf 
+#echo "RefreshActiveChecks=60" >> /etc/zabbix/zabbix_agentd.conf
+#echo "UserParameter=nginx.status[*],/etc/zabbix/ngx_status.sh \$1" >> /etc/zabbix/zabbix_agentd.conf
+#echo "UserParameter=netstat.stat[*],(netstat -ant |grep -i $1|wc -l)" >> /etc/zabbix/zabbix_agentd.conf
+#echo "UserParameter=access.status[*],/etc/zabbix/access_status.sh \$1" >> /etc/zabbix/zabbix_agentd.conf
+echo "Hostname=$NewName" >> /etc/zabbix/zabbix_agentd.conf
 systemctl enable zabbix-agent
 service zabbix-agent restart
 
-mkdir /var/log/ip_connection
-echo "*/3 * * * * sh /etc/zabbix/ip_connection_count.sh" >> /var/spool/cron/root
-service crond restart
-
-#Check
-rpm -qa | grep zabbix
-ll /etc/zabbix/
-tail -n 3 /etc/zabbix/zabbix_agentd.conf
-ll /var/log/
-cat /var/spool/cron/root
-cat /etc/zabbix/zabbix_agentd.conf | grep Hostname=
-rm -rf /root/test
-
-stsList=`cat /opt/APP/openresty/nginx/conf/vhost/*.conf | grep server_name | grep -v "#"| sed "s/ /\n/g" | grep cdn > list`
-Total=`cat list | wc -l`
-for (( i=1 ; i<=$Total ; i=i+1 ))
-do
-        HostName=`cat list | head -n $i | tail -n 1`
-        ping -c 1 -W 1 $HostName | awk '{print $3}' | tr -d "(" | tr -d ")" > Result
-        HostIP=`head -n 1 Result`
-        echo "$HostName be resolv IP : $HostIP"
-done
-
-rm -rf test list
+#mkdir /var/log/ip_connection
+#echo "*/3 * * * * sh /etc/zabbix/ip_connection_count.sh" >> /var/spool/cron/root
+#service crond restart
+#
+##Check
+#rpm -qa | grep zabbix
+#ll /etc/zabbix/
+#tail -n 3 /etc/zabbix/zabbix_agentd.conf
+#ll /var/log/
+#cat /var/spool/cron/root
+#cat /etc/zabbix/zabbix_agentd.conf | grep Hostname=
+#rm -rf /root/test
+#
+#stsList=`cat /opt/APP/openresty/nginx/conf/vhost/*.conf | grep server_name | grep -v "#"| sed "s/ /\n/g" | grep cdn > list`
+#Total=`cat list | wc -l`
+#for (( i=1 ; i<=$Total ; i=i+1 ))
+#do
+#        HostName=`cat list | head -n $i | tail -n 1`
+#        ping -c 1 -W 1 $HostName | awk '{print $3}' | tr -d "(" | tr -d ")" > Result
+#        HostIP=`head -n 1 Result`
+#        echo "$HostName be resolv IP : $HostIP"
+#done
+#
+#rm -rf test list
