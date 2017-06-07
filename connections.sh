800d7f1824df8b915d7bc6413fcb514dacc063de
nickchangs <nickchangs@gmail.com>
2017-06-07 11:11:55 +0800

    Update connections.sh

--- connections.sh
+++ connections.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+/#!/bin/bash
 #這個script是用來計算四個服務的access IP,可下參數M或m來查詢前一分鐘的資料;可下參數H或h來查詢前一小時的資料;沒有參數的話則是查詢一整天的資料
 
 case ${1} in
@@ -21,13 +21,10 @@ Total=`ls /opt/logs/nginx/*.access.log | wc -l`
         for (( i=1 ; i<=$Total ; i=i+1 ))
         do
                 LogFile=`ls /opt/logs/nginx/*.access.log | head -n $i | tail -n 1`
-                cat $LogFile | grep $TimeRange | awk '{print $3}' >> AccessIPList
-#               LogIP=`cat $LogFile | grep $TimeRange | awk '{print $3}' | sort | uniq -c | sort -n | wc -l`
-#               TotalLogIP=$((${TotalLogIP}+${LogIP}))
+                cat $LogFile | grep $TimeRange | awk '{print $3}' >> /tmp/AccessIPList
         done
-cat AccessIPList | sort | uniq -c | sort -n | wc -l
-rm -rf AccessIPList
-#echo $TotalLogIP
+cat /tmp/AccessIPList | sort | uniq -c | sort -n | wc -l
+rm -rf /tmp/AccessIPList
 }
 
 function be_AccessIP()  {
