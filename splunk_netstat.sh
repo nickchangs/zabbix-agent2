3e19ae2f69c474edea89d474657fd0d14fe58bd2
nickchangs <nickchangs@gmail.com>
2017-06-07 16:41:49 +0800

    Create splunk_netstat.sh

---
+++ splunk_netstat.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+Hostname=`cat /etc/zabbix/zabbix_agentd.conf | grep Hostname= | grep -v "#"`
+var_list=`netstat -ant |grep -i LISTEN|wc -l`
+var_ESTABLISHED=`netstat -ant |grep -i ESTABLISHED|wc -l`
+var_SYN_RECV=`netstat -ant |grep -i SYN_RECV|wc -l`
+var_SYN_SENT=`netstat -ant |grep -i SYN_SENT|wc -l`
+var_TIME_WAIT=`netstat -ant |grep -i TIME_WAIT|wc -l`
+echo "$Hostname,LISTEN=$var_list,ESTABLISHED=$var_ESTABLISHED,SYN_RECV=$var_SYN_RECV,SYN_SENT=$var_SYN_SENT,TIME_WAIT=$var_TIME_WAIT" | nc 61.216.144.184 -u 514 -w 1
