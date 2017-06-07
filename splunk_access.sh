1176ff74501f3d13735d3461042461d13138c3ec
nickchangs <nickchangs@gmail.com>
2017-06-07 18:55:11 +0800

    Update splunk_access.sh

--- splunk_access.sh
+++ splunk_access.sh
@@ -31,6 +31,11 @@ function fd_AccessIP()  {
 
 function py_AccessIP()  {
         var_ip=`cat /opt/logs/nginx/*.access.log | grep $TimeRange | awk '{print $3}' | sort | uniq -c | sort -n | wc -l`
-        echo "Type=access,$Hostname,connections=$var_ip" | nc 61.216.144.184 -u 514 -w 1
+        var_list=`netstat -ant |grep -i LISTEN|wc -l`
+        var_ESTABLISHED=`netstat -ant |grep -i ESTABLISHED|wc -l`
+        var_SYN_RECV=`netstat -ant |grep -i SYN_RECV|wc -l`
+        var_SYN_SENT=`netstat -ant |grep -i SYN_SENT|wc -l`
+        var_TIME_WAIT=`netstat -ant |grep -i TIME_WAIT|wc -l`
+        echo "Type=access,$Hostname,connections=$var_ip,LISTEN=$var_list,ESTABLISHED=$var_ESTABLISHED,SYN_RECV=$var_SYN_RECV,SYN_SENT=$var_SYN_SENT,TIME_WAIT=$var_TIME_WAIT" | nc 61.216.144.184 -u 514 -w 1
 }
 $Envir"_AccessIP"
