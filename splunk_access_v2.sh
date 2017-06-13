2998f1905eab0ce9c8f304ce1104a7ea59bfd6d5
nickchangs <nickchangs@gmail.com>
2017-06-13 10:59:32 +0800

    Create splunk_access_v2.sh

--- splunk_access_v2.sh
+++ splunk_access_v2.sh
@@ -6,18 +6,20 @@
 #請確認目前log file的大小，一定要logrotate有在執行，只存1天的LOG，其他是被壓縮起來的，不會會造成DISK I/O問題
 
 Hostname=`cat /etc/zabbix/zabbix_agentd.conf | grep Hostname= | grep -v "#"`
-                var_ip=`netstat -antu | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -n | wc -l`
-                var_list=`netstat -antu |grep -i LISTEN|wc -l`
-                var_ESTABLISHED=`netstat -antu |grep -i ESTABLISHED|wc -l`
-                var_SYN_RECV=`netstat -antu |grep -i SYN_RECV|wc -l`
-                var_SYN_SENT=`netstat -antu |grep -i SYN_SENT|wc -l`
-                var_TIME_WAIT=`netstat -antu |grep -i TIME_WAIT|wc -l`
-                var_CLOSE_WAIT=`netstat -antu |grep -i CLOSE_WAIT|wc -l`
-                var_FIN_WAIT1=`netstat -antu |grep -i FIN_WAIT1|wc -l`
-                var_FIN_WAIT2=`netstat -antu |grep -i FIN_WAIT2|wc -l`
-                var_CLOSING=`netstat -antu |grep -i CLOSING|wc -l`
-                var_Foreign=`netstat -antu |grep -i Foreign|wc -l`
-                var_LAST_ACK=`netstat -antu |grep -i LAST_ACK|wc -l`
-                #var_top_IPs=`netstat -ntu | awk ' $5 ~ /^[0-9]/ {print $5}' | cut -d: -f1 | sort | uniq -c | sort -nr | head -n10 | awk '{print $2}'| tr  '\n'  ' '`
-                var_top_ip=`netstat -antu | awk ' $5 ~ /^[0-9]/ {print $5}' | cut -d: -f1 | sort | uniq -c | sort -nr | head -n1 | awk '{print $2}'`
+netstat -antu > /tmp/netstat.log
+LogFile='/tmp/netstat.log'
+                var_ip=`cat $LogFile |awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -n | wc -l`
+                var_list=`cat $LogFile |grep -i LISTEN|wc -l`
+                var_ESTABLISHED=`cat $LogFile |grep -i ESTABLISHED|wc -l`
+                var_SYN_RECV=`cat $LogFile |grep -i SYN_RECV|wc -l`
+                var_SYN_SENT=`cat $LogFile |grep -i SYN_SENT|wc -l`
+                var_TIME_WAIT=`cat $LogFile |grep -i TIME_WAIT|wc -l`
+                var_CLOSE_WAIT=`cat $LogFile |grep -i CLOSE_WAIT|wc -l`
+                var_FIN_WAIT1=`cat $LogFile |grep -i FIN_WAIT1|wc -l`
+                var_FIN_WAIT2=`cat $LogFile |grep -i FIN_WAIT2|wc -l`
+                var_CLOSING=`cat $LogFile |grep -i CLOSING|wc -l`
+                var_Foreign=`cat $LogFile |grep -i Foreign|wc -l`
+                var_LAST_ACK=`cat $LogFile |grep -i LAST_ACK|wc -l`
+                #var_top_IPs=`cat $LogFile |awk ' $5 ~ /^[0-9]/ {print $5}' | cut -d: -f1 | sort | uniq -c | sort -nr | head -n10 | awk '{print $2}'| tr  '\n'  ' '`
+                var_top_ip=`cat $LogFile |awk ' $5 ~ /^[0-9]/ {print $5}' | cut -d: -f1 | sort | uniq -c | sort -nr | head -n1 | awk '{print $2}'`
 echo "Type=access,$Hostname,connections=$var_ip,LISTEN=$var_list,ESTABLISHED=$var_ESTABLISHED,SYN_RECV=$var_SYN_RECV,SYN_SENT=$var_SYN_SENT,LAST_ACK=$var_LAST_ACK,TIME_WAIT=$var_TIME_WAIT,CLOSE_WAIT=$var_CLOSE_WAIT,FIN_WAIT1=$var_FIN_WAIT1,FIN_WAIT2=$var_FIN_WAIT2,CLOSING=$var_CLOSING,Foreign=$var_Foreign,top_IP=$var_top_ip" | nc 61.216.144.184 -u 514 -w 1
