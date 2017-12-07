342c64c5a8de44d051601096dfb23a83d7344c2d
nickchangs <nickchangs@gmail.com>
2017-12-07 16:16:10 +0800

    Update httping.sh

--- httping.sh
+++ httping.sh
@@ -1,15 +1,17 @@
-#!/bin/bash  
+#!/bin/bash
+
 metric=$1  
 host=$2  
-port=$3  
-proto=$4  
+#port=$3  
+#proto=$4  
 tmp_file=/tmp/${metric}_httping_status.txt  
-if [ $proto == "https" ];then  
-/bin/httping -c5 -t3 -l $url > $tmp_file 
+
+#if [[ $proto == "https" ]];then  
 #/bin/httping -c5 -t3 -l $proto://$host:$port > $tmp_file  
     case $metric in  
-        status)  
-        output=$(cat $tmp_file |grep connected |wc -l )  
+        status)
+        output=`/usr/bin/httping -c5 -t3 -l $host |grep connected |wc -l`
+        #output=$(cat $tmp_file |grep connected |wc -l )
         if [ $output -eq 3 ];then  
          output=1  
         echo $output  
@@ -19,7 +21,7 @@ if [ $proto == "https" ];then
         fi  
         ;;  
         failed)  
-            output=$(cat $tmp_file |grep failed|awk '{print $5}'|awk -F'%' '{print $1}' )  
+            output=`/usr/bin/httping -c5 -t3 -l $host |grep failed|awk '{print $5}'|awk -F'%' '{print $1}'`
             if [ "$output" == "" ];then  
              echo 100  
           else  
@@ -27,7 +29,7 @@ if [ $proto == "https" ];then
           fi  
             ;;  
         min)  
-          output=$( cat $tmp_file|grep min|awk '{print $4}'|awk -F/ '{print $1}' )  
+          output=`/usr/bin/httping -c5 -t3 -l $host |grep min|awk '{print $4}'|awk -F/ '{print $1}'`
           if [ "$output" == "" ];then  
              echo 0  
           else  
@@ -35,7 +37,7 @@ if [ $proto == "https" ];then
           fi  
         ;;  
         avg)  
-            output=$(cat $tmp_file|grep avg|awk '{print $4}'|awk -F/ '{print $2}')  
+            output=`/usr/bin/httping -c5 -t3 -l $host |grep avg|awk '{print $4}'|awk -F/ '{print $2}'`
           if [ "$output" == "" ];then  
              echo 0  
           else  
@@ -43,15 +45,15 @@ if [ $proto == "https" ];then
           fi  
             ;;  
         max)  
-                output=$(cat $tmp_file|grep max|awk '{print $4}'|awk -F/ '{print $3}')  
-          if [ "$output" == "" ];then  
+                output=`/usr/bin/httping -c5 -t3 -l $host |grep max|awk '{print $4}'|awk -F/ '{print $3}'`
+          if [[ "$output" == "" ]];then  
             echo 0  
           else  
              echo $output  
           fi  
             ;;
 	loss)
-		output=$(cat $tmp_file|grep ok|awk '{print $5}' |awk -F% '{print $1}')
+		output=`/usr/bin/httping -c5 -t3 -l $host |grep ok|awk '{print $5}' |awk -F% '{print $1}'`
           if [ "$output" == "" ];then
             echo 0  
           else
@@ -61,62 +63,3 @@ if [ $proto == "https" ];then
         *)  
         echo -e "\e[033mUsage: sh  $0 [status|failed|min|avg|max]\e[0m"  
        esac  
-elif [ $proto == "http" ];then  
-    /bin/httping -c5 -t3 $proto://$host:$port > $tmp_file  
-    case $metric in  
-            status)  
-        output=$(cat $tmp_file |grep connected |wc -l )  
-        if [ $output -eq 3 ];then  
-           output=1  
-        echo $output  
-        else  
-         output=0  
-         echo   $output  
-        fi  
-        ;;  
-        failed)  
-            output=$(cat $tmp_file |grep failed|awk '{print $5}'|awk -F'%' '{print $1}' )  
-            if [ "$output" == "" ];then  
-             echo 100  
-          else  
-             echo $output  
-          fi  
-            ;;  
-        min)  
-          output=$( cat $tmp_file|grep min|awk '{print $4}'|awk -F/ '{print $1}' )  
-          if [ "$output" == "" ];then  
-             echo 0  
-          else  
-             echo $output  
-          fi  
-        ;;  
-        avg)  
-            output=$(cat $tmp_file|grep avg|awk '{print $4}'|awk -F/ '{print $2}')  
-          if [ "$output" == "" ];then  
-             echo 0  
-          else  
-             echo $output  
-          fi  
-            ;;  
-        max)  
-                output=$(cat $tmp_file|grep max|awk '{print $4}'|awk -F/ '{print $3}')  
-          if [ "$output" == "" ];then  
-             echo 0  
-          else  
-             echo $output  
-          fi  
-            ;;  
-        loss)
-                output=$(cat $tmp_file|grep ok|awk '{print $5}' |awk -F% '{print $1}') 
-          if [ "$output" == "" ];then
-             echo 0  
-          else
-             echo $output  
-          fi
-            ;;
-        *)  
-        echo -e "\e[033mUsage: sh  $0 [status|failed|min|avg|max]\e[0m"  
-       esac  
-else  
-    echo "error parm " $proto >/tmp/httping_error.log  
- fi 
