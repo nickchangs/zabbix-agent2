942689964fb92b62182650db0871d828d21a6d67
nickchangs <nickchangs@gmail.com>
2017-12-19 10:19:02 +0800

    Update ngx_status.sh

--- ngx_status.sh
+++ ngx_status.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  
-#HOST="127.0.0.1"
+HOST="localhost"
 #PORT="80"
  
 # 检测nginx进程是否存在
@@ -12,28 +12,28 @@ function failed {
 date=`date -d "1 minutes ago" | awk '{print $4}' | cut -d: -f1,2`
 cat /opt/logs/nginx/*.error.log | grep `date -d '1 minute ago' '+%H:%M'` | grep "failed" | wc -l
 }
-# 检测nginx性能
-#function active {
-#    /usr/bin/curl "http://$HOST:$PORT/ngx_status/" 2>/dev/null| grep 'Active' | awk '{print $NF}'
-#}
-#function reading {
-#    /usr/bin/curl "http://$HOST:$PORT/ngx_status/" 2>/dev/null| grep 'Reading' | awk '{print $2}'
-#}
-#function writing {
-#    /usr/bin/curl "http://$HOST:$PORT/ngx_status/" 2>/dev/null| grep 'Writing' | awk '{print $4}'
-#}
-#function waiting {
-#    /usr/bin/curl "http://$HOST:$PORT/ngx_status/" 2>/dev/null| grep 'Waiting' | awk '{print $6}'
-#}
-#function accepts {
-#    /usr/bin/curl "http://$HOST:$PORT/ngx_status/" 2>/dev/null| awk NR==3 | awk '{print $1}'
-#}
-#function handled {
-#    /usr/bin/curl "http://$HOST:$PORT/ngx_status/" 2>/dev/null| awk NR==3 | awk '{print $2}'
-#}
-#function requests {
-#    /usr/bin/curl "http://$HOST:$PORT/ngx_status/" 2>/dev/null| awk NR==3 | awk '{print $3}'
-#}
+ 检测nginx性能
+function active {
+    /usr/bin/curl "http://$HOST/nginx_status/" 2>/dev/null| grep 'Active' | awk '{print $NF}'
+}
+function reading {
+    /usr/bin/curl "http://$HOST/nginx_status/" 2>/dev/null| grep 'Reading' | awk '{print $2}'
+}
+function writing {
+    /usr/bin/curl "http://$HOST/nginx_status/" 2>/dev/null| grep 'Writing' | awk '{print $4}'
+}
+function waiting {
+    /usr/bin/curl "http://$HOST/nginx_status/" 2>/dev/null| grep 'Waiting' | awk '{print $6}'
+}
+function accepts {
+    /usr/bin/curl "http://$HOST/nginx_status/" 2>/dev/null| awk NR==3 | awk '{print $1}'
+}
+function handled {
+    /usr/bin/curl "http://$HOST/nginx_status/" 2>/dev/null| awk NR==3 | awk '{print $2}'
+}
+function requests {
+    /usr/bin/curl "http://$HOST/nginx_status/" 2>/dev/null| awk NR==3 | awk '{print $3}'
+}
 
 #check 110 failed connection time out log counter
 
